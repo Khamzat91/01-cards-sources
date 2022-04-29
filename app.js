374ef8383eef4f8cebf0b4045e9883a8e3be2962
Khamzat91 <khamzat91@mail.ru>
2022-04-30 00:19:50 +0300

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const sliderPlugin = (activeSlide = 0) => {
+  const slides = document.querySelectorAll('.slide');
+
+  slides[activeSlide].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', (e) => {
+      e.preventDefault()
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+
+  const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+}
+
+sliderPlugin()
